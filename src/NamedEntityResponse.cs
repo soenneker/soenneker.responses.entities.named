bcdc158980d15f80cf071ebfef4bec59358e56ba
Jake Soenneker <jake@soenneker.com>
2025-08-14 19:59:37 -0500

    PublicOpenApiObject

--- src/NamedEntityResponse.cs
+++ src/NamedEntityResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json.Serialization;
+using Soenneker.Attributes.PublicOpenApiObject;
 using Soenneker.Responses.Entity;
 
 namespace Soenneker.Responses.Entities.Named;
@@ -6,6 +7,7 @@ namespace Soenneker.Responses.Entities.Named;
 /// <summary>
 /// A derived EntityResponse model with a Name property, serialized using System.Text.Json property names.
 /// </summary>
+[PublicOpenApiObject]
 public record NamedEntityResponse : EntityResponse
 {
     [JsonPropertyName("name")]
